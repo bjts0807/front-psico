6edd89a492d83436d13fb9dd77e4882608e75180
sjg-dev1 <bjts0807@gmail.com>
2022-12-13 11:45:40 -0500

    feat: img login

--- src/views/LoginComponent.vue
+++ src/views/LoginComponent.vue
@@ -24,18 +24,20 @@
                 </div>
               <div class="form-floating mb-3">
                 <input type="email" class="form-control" v-model="form.email" id="floatingInput" placeholder="Email address / Username" />
-                <label for="floatingInput">Email address / Username</label>
-              </div>
-               <div v-if="v$.form.email.$error" class="text-danger" style="font-size:14px" >
-                  <i class="fa fa-warning fa-fw"></i> Por favor ingrese su e-mail.
+                <label for="floatingInput">Email</label>
+                <div v-if="v$.form.email.$error" class="text-danger" style="font-size:14px" >
+                  <i class="fa fa-warning fa-fw"></i> Por favor ingrese su email.
                 </div>
+              </div>
+               
               <div class="form-floating mb-3">
                 <input type="password" class="form-control" v-model="form.password" id="floatingInput" placeholder="Password" />
-                <label for="floatingInput">Password</label>
-              </div>
-              <div v-if="v$.form.password.$error" class="text-danger" style="font-size:14px" >
+                <label for="floatingInput">Contraseña</label>
+                <div v-if="v$.form.password.$error" class="text-danger" style="font-size:14px" >
                   <i class="fa fa-warning fa-fw"></i> Por favor ingrese su contraseña.
                 </div>
+              </div>
+              
               <!-- <div class="d-flex mt-1 justify-content-between">
                 <div class="form-check">
                   <input class="form-check-input input-primary" type="checkbox" id="customCheckc1" checked="" />
@@ -52,18 +54,11 @@
                   </div>
                </div>
                <div class="sign-bg">
-                  <svg width="280" height="230" viewBox="0 0 431 398" fill="none" xmlns="http://www.w3.org/2000/svg">
-                     <g opacity="0.05">
-                     <rect x="-157.085" y="193.773" width="543" height="77.5714" rx="38.7857" transform="rotate(-45 -157.085 193.773)" fill="#3B8AFF"/>
-                     <rect x="7.46875" y="358.327" width="543" height="77.5714" rx="38.7857" transform="rotate(-45 7.46875 358.327)" fill="#3B8AFF"/>
-                     <rect x="61.9355" y="138.545" width="310.286" height="77.5714" rx="38.7857" transform="rotate(45 61.9355 138.545)" fill="#3B8AFF"/>
-                     <rect x="62.3154" y="-190.173" width="543" height="77.5714" rx="38.7857" transform="rotate(45 62.3154 -190.173)" fill="#3B8AFF"/>
-                     </g>
-                  </svg>
+               
                </div>
             </div>
             <div class="col-md-6 d-md-block d-none bg-primary p-0 mt-n1 vh-100 overflow-hidden">
-               <!-- <img src="../assets/img/" class="img-fluid gradient-main animated-scaleX" alt="images"> -->
+               <img src="../assets/img/img-login.png" class="img-fluid gradient-main animated-scaleX" alt="images">
             </div>
          </div>
       </section>
